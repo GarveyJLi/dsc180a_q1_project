7264a0c68af5ddfa86d9b7ca4902e5a9ec17fa3a
GarveyJLi <GARVEY.J.LI@GMAIL.COM>
2024-10-30 14:08:20 -0700

    working on clumping

--- src/tools.py
+++ src/tools.py
@@ -8,8 +8,17 @@ import os
 import tarfile
 import gzip
 
-genotype_data = '../data/LDREF.tar.bz2'
-phenotype_data = '../data/GD462.GeneQuantRPKM.50FN.samplename.resk10.txt'
+
+genotype_compressed = '../data/LDREF.tar.bz2'
+phenotype_data = '../data/GD462.GeneQuantRPKM.50FN.samplename.resk10.txt.gz'
+
+def extract_data():
+    print('Extracting data...')
+    tar = tarfile.open(genotype_compressed, mode='r:bz2')
+    tar.extractall('../data')
+    tar.close()
+    print('Done')
+
 
 
 # alleles = .bim (snp. came from vcf file (txt file))
@@ -19,18 +28,16 @@ phenotype_data = '../data/GD462.GeneQuantRPKM.50FN.samplename.resk10.txt'
                              verbose=False)
 genotypes = pd.DataFrame(genotypes.compute())
 
-allele_freq_df = genotypes.copy(deep=True)
-allele_freq_df['snp'] = alleles['snp']
+#allele_freq_df = genotypes.copy(deep=True)
+#allele_freq_df['snp'] = alleles['snp']
 #allele_freq_df['freq_0'] = genotypes.apply(lambda x: x == 0).sum(axis=1) / 489
-#allele_freq_df['freq_1'] = genotypes.apply(lambda x: x == 1).sum(axis=1) / 489
-#allele_freq_df['freq_2'] = genotypes.apply(lambda x: x == 2).sum(axis=1) / 489
-allele_freq_df['eaf'] = genotypes.apply(lambda x: x != 0).sum(axis=1) / 489
-effect_allele_freq = allele_freq_df[['snp', 'eaf']]
-P = pd.read_csv(phenotype_data, sep='\t')
+#allele_freq_df['eaf'] = genotypes.apply(lambda x: x != 0).sum(axis=1) / 489
+#effect_allele_freq = allele_freq_df[['snp', 'eaf']]
+P = pd.read_csv(phenotype_data, sep='\t', compression='gzip')
+P
 
-
-def cis_eQTL_analysis(chr, gene, alleles, samples, genotypes, phenotypes, window=500000):
-    target_gene_data = phenotypes[phenotypes['Gene_Symbol']==gene].copy().reset_index(drop=True)
+def cis_eQTL_analysis(chr, gene, alleles, samples, genotypes, phenotypes, window=500000, mult_gene=False):
+    target_gene_data = phenotypes[phenotypes['Gene_Symbol']==gene].reset_index(drop=True)
     target_gene_data['start'] = target_gene_data['Coord'] - window
     target_gene_data['end'] = target_gene_data['Coord'] + window
     target_gene_alleles = alleles[(alleles['chrom'] == chr) & (alleles['pos'] <= target_gene_data['end'].values[0]) & (alleles['pos'] >= target_gene_data['start'].values[0])]
@@ -52,16 +59,9 @@ def cis_eQTL_analysis(chr, gene, alleles, samples, genotypes, phenotypes, window
         results = model.fit()
         snp_regs[snp] = results
         snp_data[snp] = {
-            #'chr': snp_meta_data['chrom'],
-            #'bp': snp_meta_data['pos'],
-            #'ea': snp_meta_data['a1'],
-            #'nea': snp_meta_data['a0'],
-            #'eaf': effect_allele_freq['freq_12'],
             'p_val': results.pvalues[1],    
             'beta': results.params[1],
             'se': results.tvalues[1],
-            't_stat': results.tvalues[1],
-            'r_sq': results.rsquared
         } 
 
     target_gene_linreg = pd.DataFrame.from_dict(snp_data, orient='index').reset_index().rename(columns={'index': 'snp'})
@@ -69,16 +69,15 @@ def cis_eQTL_analysis(chr, gene, alleles, samples, genotypes, phenotypes, window
     target_gene_linreg['gene_pos'] = target_gene_data['Coord'].values[0]
     target_gene_linreg['n'] = samples.shape[0]
     
-    
-    #target_gene_linreg['nlog10_p_val'] = -np.log10(target_gene_linreg['p_val'])
-    #target_gene_linreg = target_gene_linreg.merge(alleles[['snp', 'chrom', 'pos', 'a0', 'a1']], how='left', on='snp')
-    #target_gene_linreg = target_gene_linreg.merge(effect_allele_freq, how='left', on='snp')
-    return target_gene_linreg
+    if mult_gene:
+        return target_gene_linreg
+    return target_gene_linreg.merge(alleles[['snp', 'chrom', 'pos', 'a0', 'a1', 'i']], how='left', on='snp')
+
 
 
 
 def chr_analysis(chr, alleles, samples, genotypes, phenotypes, window=500000):
-    target_chr_data = P[P['Chr']==chr].copy()
+    target_chr_data = P[P['Chr']==chr]
     all_genes = target_chr_data['Gene_Symbol'].unique()
     all_analyses=[]
     print(f'Starting cis-eQTL analyses for chromosome {chr}')
@@ -88,9 +87,9 @@ def chr_analysis(chr, alleles, samples, genotypes, phenotypes, window=500000):
     for g in all_genes:
         if gene_counter%100==0:
             print(f'{gene_counter}/{total_genes} analyses completed')
-        temp_df = cis_eQTL_analysis(chr, g, alleles, samples, genotypes, phenotypes, window)
+        temp_df = cis_eQTL_analysis(chr, g, alleles, samples, genotypes, phenotypes, window, True)
         all_analyses.append(temp_df.copy())
         gene_counter += 1
     print(f'Compiling results')
-    return pd.concat(all_analyses).merge(pd.merge(alleles[['snp', 'chrom', 'pos', 'a0', 'a1', 'i']], effect_allele_freq, how='left', on='snp'), how='left', on='snp')
+    return pd.concat(all_analyses).merge(alleles[['snp', 'chrom', 'pos', 'a0', 'a1', 'i']], how='left', on='snp')
 
