815baf5334bbc7647ded70950786cf16e663c36a
GarveyJLi <GARVEY.J.LI@GMAIL.COM>
2024-10-31 16:07:10 -0700

    Fixing code to use plink

--- src/tools.py
+++ src/tools.py
@@ -8,33 +8,45 @@ import os
 import tarfile
 import gzip
 
-
-genotype_compressed = '../data/LDREF.tar.bz2'
+print('Loading data...')
 phenotype_data = '../data/GD462.GeneQuantRPKM.50FN.samplename.resk10.txt.gz'
-
-def extract_data():
-    print('Extracting data...')
-    tar = tarfile.open(genotype_compressed, mode='r:bz2')
-    tar.extractall('../data')
-    tar.close()
-    print('Done')
-
-
-
 # alleles = .bim (snp. came from vcf file (txt file))
 # samples = .fam (family id)
 # genotypes = .bed (genotypes). Rows and columns defined by alleles and samples
-#(alleles, samples, genotypes) = pp.read_plink("../data/LDREF/1000G.EUR.*",
-#                             verbose=False)
-#genotypes = pd.DataFrame(genotypes.compute())
+(alleles, samples, genotypes) = pp.read_plink("../data/LDREF/1000G.EUR.*",
+                             verbose=False)
+genotypes = pd.DataFrame(genotypes.compute())
+P = pd.read_csv(phenotype_data, sep='\t', compression='gzip')
+print('Complete')
 
-#allele_freq_df = genotypes.copy(deep=True)
-#allele_freq_df['snp'] = alleles['snp']
-#allele_freq_df['freq_0'] = genotypes.apply(lambda x: x == 0).sum(axis=1) / 489
-#allele_freq_df['eaf'] = genotypes.apply(lambda x: x != 0).sum(axis=1) / 489
-#effect_allele_freq = allele_freq_df[['snp', 'eaf']]
-#P = pd.read_csv(phenotype_data, sep='\t', compression='gzip')
+def get_chr(gene):
+    target_gene_info = P[P['TargetID'] == gene].iloc[0]
+    target_chr = str(target_gene_info['Chr'])
+    return target_chr
 
+def generate_pos_file(gene, out_dir='', window=500000):
+    """
+    Generates a position file from a gene to be used in get_snps() (or plink2 -extract)
+    """
+    target_gene_info = P[P['TargetID'] == gene].iloc[0]
+    target_chr = str(target_gene_info['Chr'])
+    target_start = target_gene_info['Coord'] - window
+    target_end = target_gene_info['Coord'] + window
+    coord_str = f'{target_chr}\t{target_start}\t{target_end}\t{gene}\t0\t+'
+    file_name = f'{out_dir}{gene}_coord.txt'
+    with open(file_name, "w") as text_file:
+        text_file.write(coord_str)
+    return file_name
+
+def get_snps(gene, pos_file='', out_dir='', delete_pos=True):
+    chr = get_chr(gene)
+    if pos_file == '':
+        pos_file = generate_pos_file(gene, out_dir=out_dir)
+    plink_cmd = f'./plink2 --bfile ../data/LDREF/1000G.EUR.{chr} --extract bed1 {pos_file} --out {out_dir}1000G.EUR.{chr}.{gene} --make-bed'
+    os.system(plink_cmd)
+    if delete_pos:
+        os.remove(pos_file)
+    return plink_cmd
 
 def cis_eQTL_analysis(chr, gene, alleles, samples, genotypes, phenotypes, window=500000, mult_gene=False):
     target_gene_data = phenotypes[phenotypes['Gene_Symbol']==gene].reset_index(drop=True)
@@ -46,7 +58,6 @@ def cis_eQTL_analysis(chr, gene, alleles, samples, genotypes, phenotypes, window
     sample_ids = [t for t in target_gene_data.columns if t not in ['TargetID', 'Gene_Symbol', 'Chr', 'start', 'end']]
     full_target_gene_data = target_gene_genotypes.merge(samples, how='left', left_index=True, right_index=True).merge(target_gene_data[sample_ids].T.rename(columns=snp_ids), how='left', left_on='fid', right_index=True).rename(columns={0: 'expression'}).drop(columns=['iid', 'father', 'mother', 'gender', 'trait', 'i'])
     full_target_gene_data = full_target_gene_data[~full_target_gene_data['expression'].isna()]
-
     y = full_target_gene_data['expression'].to_numpy()
     all_snps = full_target_gene_data.columns[:-2]
     snp_regs = {}
@@ -77,7 +88,7 @@ def cis_eQTL_analysis(chr, gene, alleles, samples, genotypes, phenotypes, window
 
 
 def chr_analysis(chr, alleles, samples, genotypes, phenotypes, window=500000):
-    target_chr_data = P[P['Chr']==chr]
+    target_chr_data = phenotypes[phenotypes['Chr']==chr]
     all_genes = target_chr_data['Gene_Symbol'].unique()
     all_analyses=[]
     print(f'Starting cis-eQTL analyses for chromosome {chr}')
@@ -92,4 +103,6 @@ def chr_analysis(chr, alleles, samples, genotypes, phenotypes, window=500000):
         gene_counter += 1
     print(f'Compiling results')
     return pd.concat(all_analyses).merge(alleles[['snp', 'chrom', 'pos', 'a0', 'a1', 'i']], how='left', on='snp')
+
+    #return pd.concat(all_analyses).merge(pd.merge(alleles[['snp', 'chrom', 'pos', 'a0', 'a1', 'i']], effect_allele_freq, how='left', on='snp'), how='left', on='snp')
 
