adec6f9c31685621f5c89322f510d7c857949ef2
GarveyJLi <garvey.j.li@gmail.com>
2024-12-04 22:02:59 -0800

    Cleaned demo notebooks and tool python file

--- src/tools.py
+++ src/tools.py
@@ -9,6 +9,16 @@ import os
 import tarfile
 import gzip
 
+genotype_compressed = '../data/LDREF.tar.bz2'
+phenotype_data = '../data/GD462.GeneQuantRPKM.50FN.samplename.resk10.txt.gz'
+
+def extract_data():
+    print('Extracting data...')
+    tar = tarfile.open(genotype_compressed, mode='r:bz2')
+    tar.extractall('../data')
+    tar.close()
+    print('Done')
+
 def r_squared(df, x1, x2):
     temp_r = df[x1].corr(df[x2])
     if np.isnan(temp_r):
